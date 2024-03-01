ac0211df71c5eef1fd037b197a823ff8e606463d
Yash8607 <yashmtl952@gmail.com>
2024-03-01 12:17:03 +0530

    Project Structure

---
+++ src/main/java/com/hcltech/sportique/SportiqueApplication.java
@@ -0,0 +1,13 @@
+package com.hcltech.sportique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportiqueApplication.class, args);
+	}
+
+}
